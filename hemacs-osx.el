db1c570bb9e0b64b67425a94a266cd44aed10d39
justin talbott <justin@waymondo.com>
2012-10-30 12:19:44 -0400

    using exec path from shell package, disable interprogram paste before kill

--- hemacs-osx.el
+++ hemacs-osx.el
@@ -1,12 +1,14 @@
-(defun set-exec-path-from-shell-PATH ()
-  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-     This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-  (interactive)
-  (let ((path-from-shell (string-rtrim (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
+;; (defun set-exec-path-from-shell-PATH ()
+;;   "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+;;      This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+;;   (interactive)
+;;   (let ((path-from-shell (string-rtrim (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+;;     (setenv "PATH" path-from-shell)
+;;     (setq exec-path (split-string path-from-shell path-separator))))
 
-(set-exec-path-from-shell-PATH)
+;; (set-exec-path-from-shell-PATH)
+
+(exec-path-from-shell-initialize)
 
 ;; delete to an emacs folder in trash
 (setq delete-by-moving-to-trash t)
@@ -15,9 +17,6 @@
 ;; use default osx browser
 (setq browse-url-browser-function 'browse-url-default-macosx-browser)
 
-;; font anti aliasing
-(setq mac-allow-anti-aliasing t)
-
 ;; no pop-up windows
 (setq ns-pop-up-frames nil)
 
@@ -25,10 +24,6 @@
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 (setq x-select-enable-clipboard t)
 (setq kill-do-not-save-duplicates t)
-(setq save-interprogram-paste-before-kill t)
-
-;; enhanced ruby
-;; (setq enh-ruby-program (rbenv which ruby)) ; so that still works if ruby points to ruby1.8
-;; (require 'ruby-mode)
+;; (setq save-interprogram-paste-before-kill t)
 
 (provide 'hemacs-osx)
