6e54f49da5cba66dcbe66a53c6a7ad2f33d4cda3
justin talbott <justin@waymondo.com>
2013-01-10 14:26:01 -0500

    open-file-in-github-maybe-region-highlighted binding and some magit extensions stolen from magnars

--- hemacs-git.el
+++ hemacs-git.el
@@ -1,14 +1,65 @@
 (vendor 'magit)
 (vendor 'magithub)
-;; (vendor 'gist)
 (vendor 'open-file-in-github)
 
-(define-key magit-status-mode-map (kbd "C-x C-k") 'magit-kill-file-on-line)
-(setq magit-process-popup-time 2)
+;; open file in github
+(defun open-file-in-github-maybe-region-highlighted ()
+  (interactive)
+  (if (region-active-p)
+      (open-file-in-github-region-highlighted)
+    (open-file-in-github)))
+
+(global-set-key (kbd "C-c O") 'open-file-in-github-maybe-region-highlighted)
 
 ;; always update files, i.e. after git pull
 (global-auto-revert-mode 1)
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
+
+;; pop open magit buffer when it takes more than a couple ticks
+(setq magit-process-popup-time 2)
+
+(defun magit-kill-file-on-line ()
+  "Show file on current magit line and prompt for deletion."
+  (interactive)
+  (magit-visit-item)
+  (delete-current-buffer-file)
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "C-x C-k") 'magit-kill-file-on-line)
+
+;; whitespace toggle
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
+;; full screen magit-status
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 (provide 'hemacs-git)
