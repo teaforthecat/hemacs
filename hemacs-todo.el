02a1a3b57c6ac1e4da97b4967440f2538efef1a3
justin talbott <justin@waymondo.com>
2013-01-11 15:01:30 -0500

    capture todos under project headers

--- hemacs-todo.el
+++ hemacs-todo.el
@@ -1,8 +1,21 @@
 (setq hemacs-todo-file (expand-file-name "todo.org" hemacs-dir)
       org-default-notes-file hemacs-todo-file
       org-capture-templates
-      '(("t" "Todo" entry (file+headline hemacs-todo-file "Todo")
-         "* TODO %?")))
+      '(("t" "Todo" plain (file+function hemacs-todo-file hemacs-org-capture-todo-header)
+         "** TODO %?")))
+
+(defun hemacs-org-capture-todo-header ()
+  "Find or create the current project's header and insert the todo template"
+  (goto-char (point-min))
+  (if (re-search-forward (concat "* " (persp-name persp-curr)) nil t 1)
+      (save-excursion
+        (match-string-no-properties 0)
+        (newline 1))
+    (goto-char (point-max))
+    (newline 2)
+    (insert (concat "* " (persp-name persp-curr)))
+    (newline 1)
+    ))
 
 (defun hemacs-goto-todos ()
   "Visits the global todo file."
