b6ecb6d8dc3fff82bb8792478735b69788be6aa4
justin talbott <justin@waymondo.com>
2013-01-14 12:13:44 -0500

    more semi-colon chords

--- hemacs-chords.el
+++ hemacs-chords.el
@@ -3,10 +3,16 @@
 (key-chord-mode 1)
 (key-chord-define-global "<>" 'sgml-close-tag)
 (key-chord-define-global "{}" 'open-brackets-newline-and-indent)
-(key-chord-define-global ";s" 'ido-switch-buffer)
+(key-chord-define-global "_+" 'arrow)
+
+(key-chord-define-global ";b" 'ido-switch-buffer)
+(key-chord-define-global ";r" 'recentf-ido-find-file)
 (key-chord-define-global ";f" 'ido-find-file)
 (key-chord-define-global ";g" 'magit-status)
 (key-chord-define-global ";m" 'ace-jump-mode)
-(key-chord-define-global "_+" 'arrow)
+(key-chord-define-global ";c" 'hemacs-todo-capture)
+(key-chord-define-global ";t" 'hemacs-goto-todos)
+(key-chord-define-global ";o" 'magit-in-perspective)
+(key-chord-define-global ";s" 'persp-switch)
 
 (provide 'hemacs-chords)
