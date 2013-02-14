8372302f34b6a36c8c507734a8f690f9d27f8326
justin talbott <justin@waymondo.com>
2013-02-14 14:03:55 -0500

    shell up/down comint-next/previous-matching-input-from-input

--- hemacs-shell.el
+++ hemacs-shell.el
@@ -11,20 +11,9 @@
 ;; (defun exec-use-utf8 ()
 ;;   (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
 
-;; (add-hook 'comint-exec-hook 'exec-use-utf8)
 (add-hook 'comint-mode-hook 'text-scale-decrease-one)
-
-;; (add-hook 'comint-exec-hook 'nice-shell-scrolling)
+;; (add-hook 'comint-exec-hook 'exec-use-utf8)
 ;; (add-hook 'shell-mode-hook 'turn-off-auto-fill)
-
-;; (defun nice-shell-scrolling ()
-;;   "Turn on smooth buffered scrolling"
-;;   (interactive)
-;;   (set (make-local-variable 'comint-move-point-for-output) t)
-;;   (set (make-local-variable 'scroll-margin) 0)
-;;   (set (make-local-variable 'scroll-conservatively) 0)
-;;   (set (make-local-variable 'comint-scroll-to-bottom-on-input) t)
-;;   (set (make-local-variable 'comint-scroll-show-maximum-output) t))
 
 (defun clear-shell ()
   (interactive)
@@ -33,21 +22,8 @@
     (comint-truncate-buffer)
     (setq comint-buffer-maximum-size old-max)))
 
-;; up/down to cycle through command history when on prompt line
-(defun maybe-comint-next-input (arg)
-  (interactive "p")
-  (if (looking-back (concat comint-prompt-regexp ".*"))
-      (comint-next-input arg)
-    (next-line)))
-
-(defun maybe-comint-previous-input (arg)
-  (interactive "p")
-  (if (looking-back (concat comint-prompt-regexp ".*"))
-      (comint-previous-input arg)
-    (previous-line)))
-
-(define-key comint-mode-map [down] 'maybe-comint-next-input)
-(define-key comint-mode-map [up] 'maybe-comint-previous-input)
+(define-key comint-mode-map [down] 'comint-next-matching-input-from-input)
+(define-key comint-mode-map [up] 'comint-previous-matching-input-from-input)
 (define-key comint-mode-map (kbd "C-c <C-backspace>") 'clear-shell)
 
 (provide 'hemacs-shell)
