07feb97559563b7898abd363587f8c27bb6faa74
justin talbott <justin@waymondo.com>
2013-02-25 13:31:50 -0500

    new chord arrangement

--- hemacs-chords.el
+++ hemacs-chords.el
@@ -12,17 +12,22 @@
 (key-chord-define-global ";'" 'ido-switch-buffer)
 (key-chord-define-global ";r" 'recentf-ido-find-file)
 (key-chord-define-global ";f" 'ido-find-file)
-(key-chord-define-global ";g" 'magit-status)
-(key-chord-define-global ";x" 'smex)
-(key-chord-define-global ";z" 'zap-up-to-char)
-(key-chord-define-global ";m" 'jump-char-forward)
-(key-chord-define-global ";," 'ace-jump-mode)
-(key-chord-define-global ";c" 'hemacs-todo-capture)
-(key-chord-define-global ";t" 'hemacs-goto-todos)
+(key-chord-define-global ";t" 'projectile-find-file)
+
 (key-chord-define-global ";o" 'magit-in-perspective)
 (key-chord-define-global ";s" 'persp-switch)
 (key-chord-define-global ";l" 'persp-shell)
-(key-chord-define-global ";d" 'dired-jump-other-window)
+(key-chord-define-global ";e" 'shell-here)
+(key-chord-define-global ";x" 'persp-switch-to-shell)
+
+(key-chord-define-global ";g" 'magit-status)
 (key-chord-define-global ";a" 'ack-and-a-half)
+
+(key-chord-define-global ";z" 'zap-up-to-char)
+(key-chord-define-global ";m" 'jump-char-forward)
+(key-chord-define-global ";," 'ace-jump-mode)
+
+(key-chord-define-global ";c" 'hemacs-todo-capture)
+(key-chord-define-global ";i" 'hemacs-goto-todos)
 
 (provide 'hemacs-chords)
