3ac762665e0f59df15bd71e546078a50b9315b55
justin talbott <justin@waymondo.com>
2013-04-05 02:52:11 -0400

    mmm mode, ido switch by mode in persp

--- hemacs-ido.el
+++ hemacs-ido.el
@@ -1,30 +1,48 @@
 (require 'ido-ubiquitous)
 
-;; ido
 (ido-mode t)
 (ido-everywhere t)
 (ido-ubiquitous t)
-(setq ido-enable-flex-matching t
-      ido-use-virtual-buffers t ;; include non-existent buffers
-      ido-auto-merge-work-directories-length nil
-      ido-use-filename-at-point nil
-      ido-max-window-height 14
-      ido-max-prospects 10
 
-      ;; vertical ido results!
-      ido-decorations (quote ("\n=> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(setq ido-enable-flex-matching t ;; fuzzy matching
+      ido-use-filename-at-point nil ;; don't guess based on cursor position
+      ido-max-window-height 20
+      ido-max-prospects 18)
 
 (add-to-list 'ido-ignore-files "\\.DS_Store")
 (add-to-list 'ido-ignore-files "\\.ido.last")
 (add-to-list 'ido-ignore-files "\\.loaddefs.el")
+(add-to-list 'ido-ignore-files "\\.gitkeep")
 (add-to-list 'ido-ignore-buffers "^\\*Messages\\*")
 (add-to-list 'ido-ignore-buffers "^\\*Help\\*")
-(add-to-list 'ido-ignore-buffers "^\\*Buffer")
-(add-to-list 'ido-ignore-directories "\\.git/")
 (add-to-list 'ido-ignore-directories "tmp")
 (add-to-list 'ido-ignore-directories "node_modules")
 (add-to-list 'ido-ignore-directories "vendor")
-(add-to-list 'ido-ignore-directories "public/system")
+
+;; vertical ido results!
+(setq ido-decorations (quote ("\n=> " "" "\n   " "\n   ..." "[" "]" " [No match]"
+                              " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+
+;; up/down should work as expected in vertical results
+(defun ido-up-down-fix ()
+  (define-key ido-completion-map [up] 'ido-prev-match)
+  (define-key ido-completion-map [down] 'ido-next-match))
+
+(add-hook 'ido-setup-hook 'ido-up-down-fix)
+
+;; switch buffers in project by mode
+(defun ido-for-mode-in-persp (prompt the-mode)
+  (switch-to-buffer
+   (ido-completing-read prompt
+                        (save-excursion
+                          (delq
+                           nil
+                           (mapcar (lambda (buf)
+                                     (when (buffer-live-p buf)
+                                       (with-current-buffer buf
+                                         (and (eq major-mode the-mode)
+                                              (buffer-name buf)))))
+                                   (persp-buffers persp-curr)))))))
 
 (defun recentf-ido-find-file ()
   "Find a recent file using ido."
@@ -39,19 +57,19 @@
       (find-file file))))
 
 ;; sort ido filelist by mtime instead of alphabetically
-;; (add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
-;; (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+(defun ido-sort-mtime ()
+  (setq ido-temp-list
+        (sort ido-temp-list
+              (lambda (a b)
+                (time-less-p
+                 (sixth (file-attributes (concat ido-current-directory b)))
+                 (sixth (file-attributes (concat ido-current-directory a)))))))
+  (ido-to-end  ;; move . files to end (again)
+   (delq nil (mapcar
+              (lambda (x) (and (char-equal (string-to-char x) ?.) x))
+              ido-temp-list))))
 
-;; (defun ido-sort-mtime ()
-;;   (setq ido-temp-list
-;;         (sort ido-temp-list
-;;               (lambda (a b)
-;;                 (time-less-p
-;;                  (sixth (file-attributes (concat ido-current-directory b)))
-;;                  (sixth (file-attributes (concat ido-current-directory a)))))))
-;;   (ido-to-end  ;; move . files to end (again)
-;;    (delq nil (mapcar
-;;               (lambda (x) (and (char-equal (string-to-char x) ?.) x))
-;;               ido-temp-list))))
+(add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
+(add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
 
 (provide 'hemacs-ido)
--- hemacs-templating.el
+++ hemacs-templating.el
@@ -1,15 +1,9 @@
 (require 'slim-mode)
-(require 'rhtml-mode)
+(require 'mmm-mode)
 (require 'haml-mode)
 (require 'yaml-mode)
 (require 'handlebars-mode)
 (require 'jade-mode)
-
-(add-to-list 'auto-mode-alist '("\\.hamlbars$" . haml-mode))
-(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.hbs$" . handlebars-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache$" . handlebars-mode))
 
 (define-key haml-mode-map (kbd "s-{") 'pad-brackets)
 (define-key haml-mode-map (kbd "s->") 'arrow)
