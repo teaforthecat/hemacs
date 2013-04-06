e61b1dd2fe5fdb211ecdb9c45397077d5babbecb
justin talbott <justin@waymondo.com>
2013-04-06 13:51:09 -0400

    coffeescript return modifier defuns

--- hemacs-coffee.el
+++ hemacs-coffee.el
@@ -1,9 +1,32 @@
 (require 'coffee-mode)
 
+(defun coffee-function-newline-and-indent()
+  (interactive)
+  (insert "->")
+  (coffee-newline-and-indent))
+
+(defun coffee-function-with-args()
+  (interactive)
+  (insert "() ->")
+  (coffee-newline-and-indent)
+  (previous-line)
+  (end-of-line)
+  (backward-char 4))
+
+(defun coffee-end-of-line-then-newline-and-indent ()
+  "Insert an empty line after the current line and positon
+the curson at its beginning, according to the current mode."
+  (interactive)
+  (end-of-line)
+  (coffee-newline-and-indent))
+
 (eval-after-load 'coffee-mode
   '(progn
      (electric-indent-mode -1)
      (setq coffee-tab-width 2)
+     (define-key coffee-mode-map (kbd "<s-return>") 'coffee-end-of-line-then-newline-and-indent)
+     (define-key coffee-mode-map (kbd "M-RET") 'coffee-function-newline-and-indent)
+     (define-key coffee-mode-map (kbd "<C-return>") 'coffee-function-with-args)
      (define-key coffee-mode-map (kbd "C-:") 'pad-colon-function-arrow)
      (define-key coffee-mode-map (kbd "M-:") 'pad-colon-function-arrow-arguments)
      (define-key coffee-mode-map (kbd "C-c C-c") 'coffee-compile-buffer)
