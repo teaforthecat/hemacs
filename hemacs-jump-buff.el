84b4f8c1367deb4e7ce298cf48cd4a04a91c8e5e
justin talbott <justin@waymondo.com>
2013-04-29 11:18:30 -0400

    ace-jump-buff: no bs-header in buffer switching window

--- hemacs-jump-buff.el
+++ hemacs-jump-buff.el
@@ -3,11 +3,16 @@
 (require 'perspective)
 
 ;; simple layout, increase window height
-(setq bs-max-window-height 26
-      bs-attributes-list (quote (("" 2 2 left " ")
+(setq-default ace-jump-buff-on nil
+              bs-header-lines-length 0
+              bs-max-window-height 26
+              bs-attributes-list (quote (("" 2 2 left " ")
                                  ("" 1 1 left bs--get-marked-string)
                                  ("" 1 1 left " ")
                                  ("Buffer" bs--get-name-length 10 left bs--get-name))))
+;; don't show bs header
+(defadvice bs--show-header (around maybe-disable-bs-header activate)
+  (if nil ad-do-it))
 
 ;; filter buffers to current perspective
 (add-to-list 'bs-configurations
@@ -17,17 +22,29 @@
                    (not (member buf (persp-buffers persp-curr)))))) nil)
 
 ;; on end of ace jump, select buffer
-(defun bs-ace-jump-end-hook ()
+(defun ace-jump-buff-end-hook ()
   (if (string-match (buffer-name) "*buffer-selection*")
       (bs-select)))
 
-(add-hook 'ace-jump-mode-end-hook 'bs-ace-jump-end-hook)
+(add-hook 'ace-jump-mode-end-hook 'ace-jump-buff-end-hook)
 
-(defun hemacs-jump-buff ()
+(defun ace-jump-buff ()
   (interactive)
-  (bs--show-with-configuration "persp")
+  (setq ace-jump-buff-on t)
   (setq ace-jump-mode-scope 'window)
+  (bs--show-with-configuration "persp")
   (beginning-of-buffer)
   (call-interactively 'ace-jump-line-mode))
+
+;; ;; turn off ace-jump-buff on ace-jump-done
+;; (defadvice ace-jump-done (after turn-off-ace-jump-buff-keys activate)
+;;   (setq ace-jump-buff-on nil))
+
+;; ;; escape!
+;; (defun ace-jump-buff-exit ()
+;;   (interactive)
+;;   (ace-jump-done)
+;;   (bs-kill)
+;;   (kill-buffer "*buffer-selection*"))
 
 (provide 'hemacs-jump-buff)
