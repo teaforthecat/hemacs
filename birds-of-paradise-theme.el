24771c5fc18e0a567e6c3540adee4a783b165941
justin talbott <justin@waymondo.com>
2013-07-10 01:20:56 -0400

    theme update

--- birds-of-paradise-theme.el
+++ birds-of-paradise-theme.el
@@ -7,12 +7,17 @@
 
 (custom-theme-set-faces
  'birds-of-paradise
+
  '(cursor ((t (:foreground "#865C38" :background "#E6E1C4"))))
  '(highlight ((t (:background "#1F1611" :foreground "#E6E1C4"))))
  '(hl-line ((t (:background "#3D2B28"))))
+ '(header-line ((t (:background "#372725" :box nil))))
  '(region ((t (:background "#000000"))))
  '(escape-glyph ((t (:foreground "#BE73FD"))))
  '(minibuffer-prompt ((t (:foreground "#6C99BB"))))
+ '(link ((t (:foreground "#D9D762"))))
+ '(button ((t (:foreground "#D9D762" :background "#3D2B28" :weight bold :underline t))))
+
  '(font-lock-builtin-face ((t (:foreground "#6C99BB"))))
  '(font-lock-constant-face ((t (:foreground "#6C99BB"))))
  '(font-lock-comment-face ((t (:italic t :foreground "#6B4E32"))))
@@ -26,8 +31,7 @@
  '(font-lock-type-face ((t (:bold t :foreground "#EFAC32"))))
  '(font-lock-variable-name-face ((t (:foreground "#7DAF9C"))))
  '(font-lock-warning-face ((t (:background "#990000" :foreground "white"))))
- '(link ((t (:foreground "#D9D762"))))
- '(button ((t (:foreground "#D9D762" :background "#3D2B28" :weight bold :underline t))))
+
  '(mode-line ((t (:background "#865C38" :foreground "#E6E1C4" :height 110 :box nil))))
  '(mode-line-inactive ((t (:background "#5C413D" :foreground "#E6E1C4" :height 110 :box nil))))
  '(powerline-active1 ((t (:background "#3D2B28"))))
@@ -35,14 +39,25 @@
  '(powerline-inactive1 ((t (:background "#3D2B28"))))
  '(powerline-inactive2 ((t (:background "#5C413D"))))
  '(vertical-border ((t (:foreground "#5C413D"))))
- '(fringe ((t (:background "#3D2B28" :foreground "#5C413D"))))
+ '(which-func ((t (:foreground "#E6E1C4"))))
+ '(rbnev-active-ruby-face ((t (:foreground "#E6E1C4" :weight bold :inherit nil))))
+ '(persp-selected-face ((t (:foreground "#E6E1C4" :weight bold :inherit nil))))
  '(linum ((t (:background "#3D2B28" :foreground "#5C413D"))))
+ '(linum-relative-current-face ((t (:background "#372725" :foreground "#865C38"))))
+ '(fringe ((t (:background "#3D2B28" :foreground "#5C413D" :height 100 :weight extralight))))
+ '(git-gutter-fr:added ((t (:foreground "#7DAF9C" :height 100 :weight extralight))))
+ '(git-gutter-fr:deleted ((t (:foreground "#BB99BB" :height 100 :weight extralight))))
+ '(git-gutter-fr:modified ((t (:foreground "#6C99BB" :height 100 :weight extralight))))
+ '(auto-dim-other-buffers-face ((t (:background "#302020"))))
+
  '(show-paren-match ((t (:background "#7D504A"))))
  '(show-paren-mismatch ((t (:inherit font-lock-warning-face))))
+
  '(ido-only-match ((t (:foreground "#EF5D32"))))
  '(ido-subdir ((t (:foreground "#EFAC32"))))
  '(highlight-indentation-face ((t (:background "#4B3330"))))
  '(highlight-indentation-current-column-face ((t (:background "#523D2B"))))
+
  '(whitespace-empty ((t (:background "#EFAC32"))))
  '(whitespace-hspace ((t (:foreground "#523D2B"))))
  '(whitespace-indentation ((t (:foreground "#523D2B"))))
@@ -52,26 +67,29 @@
  '(whitespace-space-after-tab ((t (:foreground "#523D2B"))))
  '(whitespace-tab ((t (:foreground "#523D2B"))))
  '(whitespace-trailing ((t (:background "#CC4232"))))
+
  '(flyspell-incorrect ((t (:underline "firebrick4"))))
  '(flyspell-duplicate ((t (:underline "firebrick4"))))
- '(compilation-info ((t (:foreground "#D9D762"))))
  '(diff-added ((t (:foreground "#8CFF8C"))))
  '(diff-removed ((t (:foreground "#FF7575"))))
  '(diff-header ((t (:background nil))))
  '(diff-hunk-header ((t (:background "#5C413D"))))
  '(diff-file-header ((t (:background "#5C413D"))))
- '(git-gutter-fr:added ((t (:foreground "#523D2B"))))
- '(git-gutter-fr:deleted ((t (:foreground "#523D2B"))))
- '(git-gutter-fr:modified ((t (:foreground "#523D2B"))))
- '(magit-diff-add ((t (:foreground "#8CFF8C"))))
+ '(success ((t (:foreground "#8CFF8C"))))
+ '(compilation-info ((t (:foreground "#D9D762"))))
+ '(compilation-mode-line-exit ((t (:foreground "#E6E1C4"))))
+ '(compilation-mode-line-run ((t (:foreground "#D9D762"))))
+ '(compilation-mode-line-fail ((t (:foreground "#FF7575"))))
  '(magit-diff-del ((t (:foreground "#FF7575"))))
  '(magit-item-highlight ((t (:background "#1F1611" :foreground nil))))
+
  '(ecb-default-highlight-face ((t (:background "#990000"))))
  '(elscreen-tab-background-face ((t (:background "#4B3330"))))
  '(elscreen-tab-control-face ((t (:background "#865C38" :foreground "#E6E1C4" :underline nil))))
  '(elscreen-tab-current-screen-face ((t (:background "#865C38" :foreground "#E6E1C4"))))
  '(elscreen-tab-other-screen-face ((t (:background "#4B3330" :foreground "#E6E1C4" :underline nil))))
  '(column-marker-1 ((t (:background "#523D2B"))))
+
  '(gnus-group-news-1 ((t (:foreground "#D9D762" :weight bold))))
  '(gnus-group-news-1-empty ((t (:foreground "#D9D762"))))
  '(gnus-group-news-2 ((t (:foreground "#EF5D32" :weight bold))))
@@ -99,6 +117,7 @@
  '(gnus-header-subject ((t (:foreground "#CC4232"))))
  '(gnus-header-name ((t (:foreground "#6C99BB"))))
  '(gnus-header-newsgroups ((t (:foreground "#EFAC32"))))
+
  '(message-header-name ((t (:foreground "#EF5D32"))))
  '(message-header-cc ((t (:foreground "#865C38"))))
  '(message-header-other ((t (:foreground "#865C38"))))
@@ -115,7 +134,7 @@
  '(nxml-cdata-section-content ((t (:foreground "#CC4232"))))
  '(nxml-attribute-local-name ((t (:foreground "#EF5D32"))))
  '(nxml-attribute-value ((t (:foreground "#D9D762"))))
- '(auto-dim-other-buffers-face ((t (:background "#302020"))))
+
  '(default ((t (:background "#372725" :foreground "#E6E1C4")))))
 
 (provide-theme 'birds-of-paradise)
