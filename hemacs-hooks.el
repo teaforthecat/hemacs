9f3a2400dc62dfcd2541a581aa4da4bcb3efbe03
justin talbott <justin@waymondo.com>
2013-07-10 01:19:14 -0400

    nice scrolling, linum mode, rainbow mode, flycheck hooks

--- hemacs-hooks.el
+++ hemacs-hooks.el
@@ -1,10 +1,12 @@
 ;; remove trailing whitespace and deactivate the mark on save
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'before-save-hook 'maybe-delete-trailing-whitespace)
 (add-hook 'before-save-hook 'maybe-deactivate-mark)
 
 ;; writing helpers
 (add-hook 'markdown-mode-hook 'visual-line-mode)
 (add-hook 'markdown-mode-hook 'flyspell-mode)
+
+(add-hook 'magit-log-edit-mode-hook 'flyspell-mode)
 
 ;; symbolism
 (add-hook 'prog-mode-hook 'pretty-lambdas)
@@ -14,18 +16,16 @@
 
 ;; turn on rainbow mode when editing themes
 (add-hook 'find-file-hook 'maybe-turn-on-rainbow-mode)
-(add-hook 'css-mode-hook 'rainbow-mode)
-(add-hook 'stylus-mode-hook 'rainbow-mode)
+(--each '(css-mode-hook stylus-mode-hook) (add-hook it 'rainbow-mode))
 
 ;; text scale down one for shell-ish modes
 (add-hook 'comint-mode-hook '(lambda ()
                                (text-scale-decrease 1)))
 
-;; force utf8 on I/O
-;; (defun exec-use-utf8 ()
-;;   (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+;; preserve scrolling buffer for programming modes
+(--each '(prog-mode-hook css-mode-hook sgml-mode-hook magit-mode-hook) (add-hook it 'nice-scrolling))
 
-;; (add-hook 'comint-exec-hook 'exec-use-utf8)
-;; (add-hook 'shell-mode-hook 'turn-off-auto-fill)
+;; show linum-mode for programming modes
+(--each '(prog-mode-hook less-css-mode-hook sgml-mode-hook) (add-hook it 'linum-mode))
 
 (provide 'hemacs-hooks)
